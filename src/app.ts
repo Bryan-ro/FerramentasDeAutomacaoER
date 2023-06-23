61faef9153917715502b6470338f79541f782df3
Bryan-ro <bryangomesrocha@gmail.com>
2023-06-23 00:41:11 -0300

    Criando primeiro admin

--- src/app.ts
+++ src/app.ts
@@ -3,10 +3,26 @@ import router from "./routes/routes";
 import { PrismaClient } from "@prisma/client";
 
 const app = express();
+const prisma = new PrismaClient();
 
+async function createFirstAdminUser () {
 
+    try {
+        await prisma.user.create({
+            data: {
+                name: "Bryan Rocha",
+                email: "bryan.rocha@extremereach.com",
+                admin: true
+            }
+        });
+    } catch (error) {
+        console.log("O úsuario admin inicial já existia.");
+    }
+
+}
 
 app.use(express.json());
 app.use(router);
+createFirstAdminUser();
 
 export default app;
