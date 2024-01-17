38efd0b1bb057efa3066801fc3b91cf9916797c9
Bryan-ro <bryangomesrocha@gmail.com>
2024-01-17 17:57:39 -0300

    sbt routes

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -1,15 +1,18 @@
 import { UserController } from "../controllers/userController";
 import { RecordBroadcasterController } from "../controllers/recordBroadcasterController";
 import { BandBroadcasterController } from "../controllers/bandBroadCasterController";
+import { SbtBroadcasterController } from "../controllers/sbtBroadcasterController";
 import { Router }  from "express";
 
 const router = Router();
 const users = new UserController();
 const recordController = new RecordBroadcasterController();
 const bandController = new BandBroadcasterController();
+const sbtController = new SbtBroadcasterController
 
 router.use("/users", users.routes());
 router.use("/record", recordController.routes());
 router.use("/band", bandController.routes());
+router.use("/sbt", sbtController.routes());
 
 export default router;
